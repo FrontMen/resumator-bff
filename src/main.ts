d43a98548cfefee96134c129a9a85302f6198867
ArmanSarkisov <arman.sarkisov.96@gmail.com>
2022-02-02 17:39:05 +0100

    prot

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT || process.env.SERVER_PORT || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
