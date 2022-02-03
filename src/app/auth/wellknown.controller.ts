4ef8b6f6478817ffbd4e2411398ce316b62c26e3
Zukzuk <dave.timmerman@gmail.com>
2022-02-03 16:37:59 +0100

    feat(): removed ACME challenge strings

--- src/app/auth/wellknown.controller.ts
+++ src/app/auth/wellknown.controller.ts
@@ -5,9 +5,9 @@ import { ApiTags } from '@nestjs/swagger';
 @Controller('.well-known')
 export class WellKnownController {
   // !* this endpoint only for certbot ACME challenge
-  @Get('/acme-challenge/L2ocJ1-B1TwoRhchf46RcrVDllmUdvtvMp2q3Ut_29E')
+  @Get('/acme-challenge/CHALLENGE-STRING')
   @Header('content-type', 'text/html')
   async certbotAcmeChallenge(@Query() query: any): Promise<any> {
-    return 'L2ocJ1-B1TwoRhchf46RcrVDllmUdvtvMp2q3Ut_29E.s2B09Sc4-eE5gL3NHeyWuY5ogmUczTv5sfuVWxI2t1A';
+    return 'RESPONSE-STRING';
   }
 }
