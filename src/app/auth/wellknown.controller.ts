49868d27bda87ce5ee5b2db062d12f56921cee2c
Zukzuk <dave.timmerman@gmail.com>
2022-02-03 15:23:41 +0100

    fix(): renewed challenge key

--- src/app/auth/wellknown.controller.ts
+++ src/app/auth/wellknown.controller.ts
@@ -2,12 +2,12 @@ import { Controller, Get, Header, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('.well-known')
-@Controller('.wellknown')
+@Controller('.well-known')
 export class WellKnownController {
-  // !* this endpoint only for certbot SSL verification
-  @Get('/acme-challenge/SQ1P6LefGguadKnCbBn4GXetCXb5T-80CArQk6TEdJk')
+  // !* this endpoint only for certbot ACME challenge
+  @Get('/acme-challenge/L2ocJ1-B1TwoRhchf46RcrVDllmUdvtvMp2q3Ut_29E')
   @Header('content-type', 'text/html')
   async certbotAcmeChallenge(@Query() query: any): Promise<any> {
-    return 'SQ1P6LefGguadKnCbBn4GXetCXb5T-80CArQk6TEdJk.s2B09Sc4-eE5gL3NHeyWuY5ogmUczTv5sfuVWxI2t1A';
+    return 'L2ocJ1-B1TwoRhchf46RcrVDllmUdvtvMp2q3Ut_29E.s2B09Sc4-eE5gL3NHeyWuY5ogmUczTv5sfuVWxI2t1A';
   }
 }
